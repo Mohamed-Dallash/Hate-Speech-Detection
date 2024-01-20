7395cb7c84fa14e6e107738ed7cad71a1bbba343
Mohamed Ayman <mohameddallash21@gmail.com>
2024-01-20 14:41:58 +0200

    Add second dataset

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -52,6 +52,27 @@ def remove_contractions(df : pd.DataFrame, col_name : str):
 
     df[col_name] = df[col_name].apply(lambda text: ' '.join([contractions.fix(word) for word in str(text).split()]))
 
+def remove_emojis(df : pd.DataFrame, col_name : str):
+    emoji_pattern = re.compile("["
+          u"\U0001F600-\U0001F64F"  # emoticons
+          u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+          u"\U0001F680-\U0001F6FF"  # transport & map symbols
+          u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+          u"\U00002702-\U000027B0"
+          u"\U000024C2-\U0001F251"
+          u"\U0001f926-\U0001f937"
+          u'\U00010000-\U0010ffff'
+          u"\u200d"
+          u"\u2640-\u2642"
+          u"\u2600-\u2B55"
+          u"\u23cf"
+          u"\u23e9"
+          u"\u231a"
+          u"\u3030"
+          u"\ufe0f"
+                            "]+", flags=re.UNICODE)
+    df[col_name] = df[col_name].apply(lambda text: emoji_pattern.sub(r'', str(text)))
+
 def remove_stopwords(df : pd.DataFrame, col_name : str):
     def stopwords_remover(text):
         stop_words = stopwords.words('english')
---
+++ Second_Dataset.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from Data_Preprocessing import *
+
+class Second_Dataset:
+    def __init__(self) -> None:
+        self.df = pd.read_csv('hasoc_english_dataset.tsv',sep="\t")
+    
+    def preprocess(self):
+        remove_retweet_label(self.df,"text")
+        remove_URL(self.df,"text")
+        replaceAtUser(self.df,"text")
+        remove_contractions(self.df,"text")
+        convertLowerCase(self.df,"text")
+        remove_emojis(self.df,"text")
+        remove_punctuation(self.df,'text')
+        remove_at_symbols(self.df,'text')
+        remove_numbers(self.df,"text")
+        remove_stopwords(self.df, 'text')
+    
+    def split(self, val_size = 0.1, test_size = 0.1):
+        features = self.embedded_tweets
+        target = self.encoded_labels
+        self.X_train, self.X_val, self.X_test, self.Y_train, self.Y_val, self.Y_test = split_data(features,target,val_size,test_size)
+    
+    def encode_labels(self):
+        self.encoded_labels = one_hot_encode(self.df['task_1'])
+
+    def embed_tokens(self, max_words, max_len):
+        self.embedded_tweets = generate_token_embeddings(self.df['text'], max_words, max_len)
+
+    def getData(self):
+        return self.X_train, self.X_val, self.X_test, self.Y_train, self.Y_val, self.Y_test
